60ea848db103e78ef36abae8883ebb8fe1d37f2a
Subhajit <43522742+Subho-n5@users.noreply.github.com>
2021-11-22 20:29:28 +0530

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,6 +1,9 @@
 wget https://golang.org/dl/go1.17.3.linux-amd64.tar.gz
+sleep 10
+:'
 tar -xf go1.17.3.linux-amd64.tar.gz
 mv go /usr/local/
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
 go version
+'
